99179c323ba95876798ea06afd0b4df806582f24
ryanbrownCSIWeb <43590551+ryanbrownCSIWeb@users.noreply.github.com>
2019-11-25 10:16:05 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace TwistCLI
     {
         public static void Main(string[] args)
         {
-            string address = "https://104.43.131.93:8083";
+            string address = "https://13.89.222.174:8083/";
             string username = "ryan.brown@csiweb.com";
             string password = args[0];
             string dir = System.IO.Directory.GetCurrentDirectory();
